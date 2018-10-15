126f56ce30f9c217b102fd6a438ee53bc77f9321
Angry Wasp <angrywasp@live.com>
2018-10-16 00:18:49 +1100

    Add endpoint to get mining status

---
+++ getminingstatus.php
@@ -0,0 +1,15 @@
+<?php 
+require_once('./lib/config.php');
+
+
+$ch = curl_init();
+$url = 'http://'.HOST.':'.PORT.'/mining_status';
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$obj=curl_exec($ch);
+curl_close($ch);
+
+echo $obj;
+?>
