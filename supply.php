a69bfcb1f51cf2b23d62f9846a8bf4e4d5605c84
Angry Wasp <angrywasp@live.com>
2018-11-08 10:45:37 +1100

    Add endpoint for cmc supply

---
+++ supply.php
@@ -0,0 +1,8 @@
+<?php 
+require_once('./lib/config.php');
+require_once('./lib/helper.php');
+
+$json = send_request(HOST, PORT, "get_generated_coins", null);
+$c = json_decode($json)->result->coins;
+echo ($c / 1000000000000);
+?>
