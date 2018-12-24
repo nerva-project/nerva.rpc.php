248fd64f9bdf79a1af8cc2135010a0a8aa305a0e
Angry Wasp <angrywasp@live.com>
2018-12-25 10:33:51 +1100

    Typo

--- setbans.php
+++ setbans.php
@@ -2,7 +2,7 @@
 require_once('./lib/config.php');
 require_once('./lib/helper.php');
 
-$pre = '{"jsonrpc":"2.0","id":"0","method":"get_bans","params":{"bans":[{"host":"'.$_GET["ip"].'","ban":'.$_GET["ban"].',"seconds":'.$_GET["time"].'}]}}';
+$pre = '{"jsonrpc":"2.0","id":"0","method":"set_bans","params":{"bans":[{"host":"'.$_GET["ip"].'","ban":'.$_GET["ban"].',"seconds":'.$_GET["time"].'}]}}';
 $json = send_request_preformatted(HOST, PORT, "set_bans", $pre);
 echo $json;
 ?>
