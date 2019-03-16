207d3852617889e8ac728892085fa1a2015dea3f
Angry Wasp <angrywasp@live.com>
2019-03-16 11:27:33 +1100

    New endpoints. HTML page for explorer

--- getblocktemplate.php
+++ getblocktemplate.php
@@ -5,6 +5,21 @@
 require_once('./lib/config.php');
 require_once('./lib/helper.php');
 
+$error = false;
+
+if (!isset($_GET["address"])) {
+    echo "Need parameter 'address'\n";
+    $error = true;
+}
+
+if (!isset($_GET["reserve_size"])) {
+    echo "Need parameter 'reserve_size'\n";
+    $error = true;
+}
+
+if ($error)
+    exit;
+
 $params = array(
     "wallet_address" => $_GET["address"],
     "reserve_size" => $_GET["res"],
