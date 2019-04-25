83d7e5b1d8c7b08d1ef12572747511bdadc25ad8
Angry Wasp <angrywasp@live.com>
2019-04-25 16:51:28 +1000

    Add endpoint to get a node peer list

--- lib/config.php
+++ lib/config.php
@@ -1,8 +1,8 @@
 <?php 
 define('HOST', '127.0.0.1');
-define('PORT', 22424);
+define('PORT', 18566);
 define('ANALYTICS_HOST', HOST);
-define('ANALYTICS_PORT', 12345);
+define('ANALYTICS_PORT', 15236);
 define('DEBUG', true);
 
 header('Content-Type: application/json');
